9392acf084b8aaa4827e8bda9f462c6d62982f67
Yura Shkoropad <lvivgeorge@gmail.com>
2018-01-22 22:17:21 +0200

    Updated configs

--- config/server.js
+++ config/server.js
@@ -1,8 +1,9 @@
 const PORT = 8000;
+const IP_ADDRESS = '127.0.0.1';
 const CSP_DIRECTIVES = [
   'fonts.googleapis.com', 'fonts.gstatic.com'
 ];
 
 module.exports = {
-  PORT, CSP_DIRECTIVES
+  PORT, CSP_DIRECTIVES, IP_ADDRESS
 };
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const https = require('https');
 const app = require('./app');
-const { PORT } = require('config/server');
+const { PORT, IP_ADDRESS } = require('config/server');
 const certificates = require('./certificates');
 
 const serverOptions = {...certificates};
 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", port " + server_port )
+});
+
 https
   .createServer(serverOptions, app)
-  .listen(PORT, err => {
+  .listen(PORT, IP_ADDRESS, err => {
     if (err) throw err;
     console.log(`Server started at port: ${ PORT }`)
   });

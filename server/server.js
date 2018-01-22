849ce93d46c12f16bec2fbfa9d78dc1c9cb8f3db
Yura Shkoropad <lvivgeorge@gmail.com>
2018-01-22 22:26:18 +0200

    Added ProcFile

--- server/server.js
+++ server/server.js
@@ -5,10 +5,6 @@ const certificates = require('./certificates');
 
 const serverOptions = {...certificates};
 
-server.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
-
 https
   .createServer(serverOptions, app)
   .listen(PORT, IP_ADDRESS, err => {

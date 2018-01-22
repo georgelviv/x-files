cde964a571454f1921a60481cb197edc9a7af55b
Yura Shkoropad <lvivgeorge@gmail.com>
2018-01-22 23:41:30 +0200

    Added httP

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-const https = require('https');
 const app = require('./app');
 const { PORT, IP_ADDRESS } = require('config/server');
 const certificates = require('./certificates');
 
 const serverOptions = {...certificates};
 
-https
-  .createServer(serverOptions, app)
+const server = !process.env.IS_HEROKU 
+  ? require('https').createServer(serverOptions, app)
+  : require('http').createServer(app);
+
+server
   .listen(PORT, err => {
     if (err) throw err;
     console.log(`Server started at port: ${ PORT }`)
